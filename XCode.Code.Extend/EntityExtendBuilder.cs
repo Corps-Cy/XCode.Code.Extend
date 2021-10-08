dbdfab8b6e317104c92559f3caa77935c3d0bde4
ssk.cy <1143381890@qq.com>
2021-10-08 18:21:50 +0800

    upload

---
+++ XCode.Code.Extend/EntityExtendBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using XCode.DataAccessLayer;
+
+namespace XCode.Code.Extend
+{
+    /// <summary>
+    /// Crops 扩展T4代码生成器
+    /// </summary>
+    public class EntityExtendBuilder: EntityBuilder
+    {
+        public static int Build()
+        {
+            IDictionary<string, string> atts;
+            IList<IDataTable> tables = ClassBuilder.LoadModels(null, null, out atts);
+            EntityBuilder.FixModelFile(null, null, atts, tables);
+            return EntityBuilder.BuildTables(tables, null,true);
+        }
+        protected override void BuildBiz()
+        {
+            this.WriteLine("========测试操作==========");
+            base.BuildBiz();
+        }
+    }
+}
